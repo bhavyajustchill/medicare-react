d7fe58bccc2f8069f75c59f7ca6cbb0163078675
Bhavya Popat <bhavyabpopat7777@gmail.com>
2023-05-10 08:46:37 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Dashboard from "./components/Dashboard";
+import AdminLogin from "./components/AdminLogin";
+import AdminRegister from "./components/AdminRegister";
+import AdminProfile from "./components/AdminProfile";
+import MedicineCategory from "./components/MedicineCategory";
+import AddCategory from "./components/AddCategory";
+import UpdateCategory from "./components/UpdateCategory";
+import MedicineTypes from "./components/MedicineTypes";
+import AddType from "./components/AddType";
+import UpdateType from "./components/UpdateType";
+import Inventory from "./components/Inventory";
+import AddMedicine from "./components/AddMedicine";
+import UpdateMedicine from "./components/UpdateMedicine";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={<Dashboard />} />
+        <Route exact path="/login" element={<AdminLogin />} />
+        <Route exact path="/register" element={<AdminRegister />} />
+        <Route exact path="/profile" element={<AdminProfile />} />
+        <Route exact path="/categories" element={<MedicineCategory />} />
+        <Route exact path="/addcategory" element={<AddCategory />} />
+        <Route exact path="/updatecategory" element={<UpdateCategory />} />
+        <Route exact path="/types" element={<MedicineTypes />} />
+        <Route exact path="/addtype" element={<AddType />} />
+        <Route exact path="/updatetype" element={<UpdateType />} />
+        <Route exact path="/inventory" element={<Inventory />} />
+        <Route exact path="/addmedicine" element={<AddMedicine />} />
+        <Route exact path="/updatemedicine" element={<UpdateMedicine />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
 export default App;
+
--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter } from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
@@ -15,3 +16,4 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
